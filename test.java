6b4750b70e92ffd90e9ed7ecee3eb9c95dd283f7
Jatin Bhutka <jatinbhutka@gmail.com>
2025-02-01 23:48:18 -0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,36 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class RomanNumeralConverter {
+
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(symbols[i]);
+            }
+        }
+        return result.toString();
+    }
+
+    @GetMapping("/convert")
+    public String convert(@RequestParam int num) {
+        if (num <= 0 || num > 3999) {
+            return "Error: Number must be between 1 and 3999";
+        }
+        return intToRoman(num);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomanNumeralConverter.class, args);
+    }
+}
